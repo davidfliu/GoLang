528e065810f67b4c5c0d56f1d0522619d1f082e3
David Liu <rumalzliu@gmail.com>
2016-06-14 08:17:35 -0400

    initial commit

---
+++ loops.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	var a int
+
+	for a :=0; a < 10; a ++ {
+
+		fmt.Printf("Value of a is: %d\n", a)
+	}
+
+	fmt.Printf("Final value of a is: %d\n", a)
+
+
+}

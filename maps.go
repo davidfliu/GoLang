bb4dc1c4cd0e876ac44a3a87b6b337e5ba6b6929
davidfliu <rumalzliu@gmail.com>
2016-06-17 11:52:50 -0400

    added maps.go

---
+++ maps.go
@@ -0,0 +1,25 @@
+//Learning about the maps data type.
+//Maps keys to values.
+
+package main 
+
+import "fmt"
+
+func main() {
+
+	//variable declaration with make function.
+	var mapVar map[string]string
+	mapVar = make(map[string]string)
+
+	//Key-value pairs.
+	mapVar["Apples"] = "Red"
+	mapVar["Bananas"] = "Yellow"
+	mapVar["Oranges"] = "Orange"
+
+	//Print out all key-value pairs.
+	for object := range mapVar {
+		fmt.Println("Color of", object, "is", mapVar[object])
+	} 
+
+}
+

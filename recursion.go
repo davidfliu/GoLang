8394c8eaf476bffc1fc4eb4636c5f4045dbe84b2
David Liu <rumalzliu@gmail.com>
2016-06-18 23:52:14 -0400

    updated styling

--- recursion.go
+++ recursion.go
@@ -8,15 +8,20 @@ func Factorial(i int)(result int) {
 
 	//Terminating case.
 	if (i > 1) {
+
 		result = i * Factorial(i - 1)
+		
 		return result
 	}
+
 	return 1
 
 }
 
+//Main function.
 func main() {
 
 	i := 5
+
 	fmt.Printf("Factorial of %d is: %d\n", i, Factorial(i))
 }
